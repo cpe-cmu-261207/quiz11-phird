03adf3861933b44b3fa05ac3f1f32ac6a40a0912
phird <iamphird@gmail.com>
2021-01-12 13:57:19 +0700

    TT

--- app_template.js
+++ app_template.js
@@ -1,1 +1,27 @@
 //your code here!
+const inputNumber = document.querySelector("#input-number");
+const compute = document.querySelector("#button-compute");
+const createTable = document.querySelector("#button-create-table");
+
+
+
+compute.addEventListener("click", () => {
+    alert("i am in button");
+    let id = 620610801 ;
+    if (
+        inputNumber.value === 0
+    ) {
+        alert("Please input Number !");
+    }
+    else{
+
+        const tr = document.createElement("tr");
+        const tdOutput = document.createElement("td");
+        tdOutput.innerHTML = inputNumber.value;
+
+        tdAnswer.innerHTML = id - inputNumber.value ;
+        alert(tdAnswer)
+    }
+} )
+
+
